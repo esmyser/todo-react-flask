8cfde38b9efd81459a7868641dc4e671f733b4a7
esmyser <esmyser@gmail.com>
2016-12-11 23:33:12 -0500

    connects to heroku postgresql

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, render_template
 
+import os
+import psycopg2
+import urllib.parse
+
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET, POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # if request.method == 'POST':
     #   createItem(request.form)
