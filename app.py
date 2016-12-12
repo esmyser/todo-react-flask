c18f2cf8843ab526c79d62e0ee9a5006b419028c
esmyser <esmyser@gmail.com>
2016-12-12 14:07:45 -0500

    adds SQLAlchemy db connection

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask, request, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 import os
 import psycopg2
 import urllib.parse
 
-urllib.parse.uses_netloc.append("postgres")
-url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -28,14 +20,14 @@ def index():
 # def show_item():
 #     return "placeholder"
 
-@app.route('/<int:item_id>/edit', methods=['PUT'])
-def edit_item():
-    return "placeholder"
+# @app.route('/<int:item_id>/edit', methods=['PUT'])
+# def edit_item():
+#     return "placeholder"
 
-@app.route('/<int:item_id>/finish', methods=['PUT'])
-def finish_item():
-    return "placeholder"
+# @app.route('/<int:item_id>/finish', methods=['PUT'])
+# def finish_item():
+#     return "placeholder"
 
-@app.route('/<int:item_id>/delete', methods=['DELETE'])
-def delete_item():
-    return "placeholder"
+# @app.route('/<int:item_id>/delete', methods=['DELETE'])
+# def delete_item():
+#     return "placeholder"
