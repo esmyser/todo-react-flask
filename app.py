b2614f1f9177146a1c26fc45c3028abdb998e257
esmyser <esmyser@gmail.com>
2016-12-11 22:20:19 -0500

    adds crud routes for todo items

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET, POST'])
+def index():
+    # if request.method == 'POST':
+    #   createItem(request.form)
+    # else
     return render_template('index.html')
+
+# @app.route('/<int:item_id>')
+# def show_item():
+#     return "placeholder"
+
+@app.route('/<int:item_id>/edit', methods=['PUT'])
+def edit_item():
+    return "placeholder"
+
+@app.route('/<int:item_id>/finish', methods=['PUT'])
+def finish_item():
+    return "placeholder"
+
+@app.route('/<int:item_id>/delete', methods=['DELETE'])
+def delete_item():
+    return "placeholder"
