c1e178cb23650b8e420388a9592336366dd8e95a
esmyser <esmyser@gmail.com>
2016-12-18 12:02:09 -0500

    reducer has more complex initial state to handle async actions

--- react-redux-frontend/src/reducers/todoList.js
+++ react-redux-frontend/src/reducers/todoList.js
@@ -1,9 +1,31 @@
 import todo from './todo';
 
-const todoList = (state=[], action) => {
+// updating the state from just an array of items
+// to a json object like so: { items: [], isFetching: false, lastUpdated: null }
+// 
+const initialState = { 
+    todos: [], 
+    isFetching: false, 
+    lastUpdated: null 
+};
+
+const todoList = (state=initialState, action) => {
     switch (action.type) {
+        case 'REQUEST_TODOS':
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case 'RECEIVE_TODOS':
+            return Object.assign({}, state, {
+                todos: action.todos,
+                isFetching: false,
+                lastUpdated: action.receivedAt
+            });
         case 'ADD_TODO':
-            return [...state, todo(undefined, action)];
+            return Object.assign({}, state, {
+                todos: [...state, todo(undefined, action)],
+                isFetching: false
+            });
         case 'TOGGLE_TODO':
             return state.map(t => todo(t, action));
         default:
