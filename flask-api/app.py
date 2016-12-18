80bb2f68518b61d54f6d375b90eb0ba0ec903cdf
esmyser <esmyser@gmail.com>
2016-12-18 14:27:01 -0500

    stores items in memory instead of in db - temporary while hooking up frontend to backend

--- flask-api/app.py
+++ flask-api/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, redirect, url_for, render_template
 # import os
 import psycopg2
 import urllib.parse
+import uuid
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -12,17 +13,26 @@ app = Flask(__name__)
 
 # from models import Item
 
+items = []
+
 @app.route('/todos', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        item = Item(request.form['description'])
-        db.session.add(item)
-        db.session.commit()
+        # item = Item(request.form['text'])
+        # db.session.add(item)
+        # db.session.commit()
         print("adding the item: ")
+        item = {
+            id: uuid.uuid4(),
+            text: request.form['text'],
+            completed: False,
+            order: len(items) + 1
+        }
+        items.append(item)
         print(item)
         return jsonify(item)
     else:
-        items = Item.query.all()
+        # items = Item.query.all()
         print("getting all items: ")
         print(items)
 
